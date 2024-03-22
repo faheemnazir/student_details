a55944d003533ef92414ae0452ca45e7e3c93490
FAHEEM NAZIR <1rn19cs047.faheemnazir@gmail.com>
2024-03-22 10:00:56 +0530

    first commit in creation of student

---
+++ craeteStudent.cs
@@ -0,0 +1,13 @@
+namespace student_classes;
+
+
+    public class craeteStudent
+    {
+        public int id{ get; set; }
+        public string name{ get; set; }
+        public long phone{ get; set; }
+        public string address{ get; set; }
+    
+  
+    
+    }
